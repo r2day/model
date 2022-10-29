f1d864404a00ba3b76671b9215cabd2a2a576027
reday <redayxu@tencent.com>
2022-10-30 00:34:37 +0800

    add index for merchant

--- member.go
+++ member.go
@@ -10,7 +10,7 @@ type MemberInfo struct {
 	// Id 自增唯一id
 	Id uint `json:"id" gorm:"unique"`
 	// MerchantId 商户ID (例如: 黄李记作为一个商户存在)
-	MerchantId string `json:"merchant_id" `
+	MerchantId string `json:"merchant_id" gorm:"index:idx_merchant"`
 	// Status 状态
 	Status string `gorm:"default:effected" gorm:"index:idx_status"`
 	// CreatedAt 创建时间
