dc868156a2fa75e1868fdbd1a7c964ca2831d673
reday <redayxu@tencent.com>
2022-11-09 23:47:26 +0800

    add reviews

--- reviews.go
+++ reviews.go
@@ -27,6 +27,49 @@ func (m Reviews) All(instance interface{}) error {
 	return nil
 }
 
+// ListByOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m Reviews) ListByOffset(instance interface{}, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+
+	err := m.counter("reviews", &counter)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+
+	// 获取列表
+	err = m.listByOffset("reviews", instance, offset, limit)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
+// ListByFilterOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m Reviews) ListByFilterOffset(instance interface{}, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+
+	filter := " and status = ?"
+	filterParams := make([]string, 0)
+	filterParams = append(filterParams, m.Status)
+
+	err := m.counterByFilter("reviews", &counter, filter, filterParams)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+	// 获取列表
+	err = m.listByFilterOffset("reviews", instance, offset, limit, filter, filterParams)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
 // GetOne 获取单个数据
 // 以便管理员进行审核操作
 func (m Reviews) GetOne(instance interface{}) error {
