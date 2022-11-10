1129448a002ebd2751953eb3cef960af77318ce4
reday <redayxu@tencent.com>
2022-11-10 10:29:37 +0800

    add categories

---
+++ categories.go
@@ -0,0 +1,81 @@
+package model
+
+// Categories 商品分类
+type Categories struct {
+	BaseModel
+
+	Name string `json:"name"`
+
+	Status string `json:"status"`
+}
+
+// All 获取所有数据
+func (m Categories) All(instance interface{}) error {
+	err := m.all("categories", instance)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListByOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m Categories) ListByOffset(instance interface{}, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+
+	err := m.counter("categories", &counter)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+
+	// 获取列表
+	err = m.listByOffset("categories", instance, offset, limit)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
+// GetOne 获取单个数据
+// 以便管理员进行审核操作
+func (m Categories) GetOne(instance interface{}) error {
+	err := m.getOne("categories", instance)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListByFilterOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m Categories) ListByFilterOffset(instance interface{}, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+
+	filter := " and status = ?"
+	filterParams := make([]string, 0)
+	filterParams = append(filterParams, m.Status)
+
+	err := m.counterByFilter("categories", &counter, filter, filterParams)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+	// 获取列表
+	err = m.listByFilterOffset("categories", instance, offset, limit, filter, filterParams)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
+// Delete 删除记录
+func (m Categories) Delete() error {
+	err := m.delete("categories")
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- init.go
+++ init.go
@@ -47,6 +47,7 @@ func InitDataBase(dsn string, p logger.Interface, debug bool) error {
 	DataHandler.AutoMigrate(&Commands{})
 	DataHandler.AutoMigrate(&Invoices{})
 	DataHandler.AutoMigrate(&Reviews{})
+	DataHandler.AutoMigrate(&Categories{})
 
 	// 用户管理(用于管理当前系统的用户权限)
 	// 商户号申请
