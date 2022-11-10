f0395803c1126265ce6c13983ff8349d194d13f1
reday <redayxu@tencent.com>
2022-11-10 21:52:34 +0800

    update customers

--- customers.go
+++ customers.go
@@ -119,3 +119,30 @@ func (m Customers) GetOne(instance interface{}) error {
 	}
 	return nil
 }
+
+// ListByFilterOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m Customers) ListByFilterOffset(instance interface{}, filter []string, filterParams []string, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+	err := m.counterByFilter("customers", &counter, filter, filterParams)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+	// 获取列表
+	err = m.listByFilterOffset("customers", instance, offset, limit, filter, filterParams)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
+// Delete 删除记录
+func (m Customers) Delete() error {
+	err := m.delete("customers")
+	if err != nil {
+		return err
+	}
+	return nil
+}
