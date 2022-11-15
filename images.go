c7306a4f110cfed74a17eff4dc45c6e7d2cc63a6
reday <redayxu@tencent.com>
2022-11-16 01:19:48 +0800

    add images

--- images.go
+++ images.go
@@ -46,6 +46,24 @@ func (m *Images) ListByOffset(instance interface{}, offset int, limit int) (int6
 	return counter, nil
 }
 
+// ListByFilterOffset 获取所有数据
+// 以便管理员进行审核操作
+func (m *Images) ListByFilterOffset(instance interface{}, filter []string, filterParams []string, offset int, limit int) (int64, error) {
+	var counter int64 = 0
+	err := m.counterByFilter("images", &counter, filter, filterParams)
+	if err != nil {
+		return 0, err
+	} else if counter == 0 {
+		return 0, nil
+	}
+	// 获取列表
+	err = m.listByFilterOffset("images", instance, offset, limit, filter, filterParams)
+	if err != nil {
+		return 0, err
+	}
+	return counter, nil
+}
+
 // GetOne 获取单个数据
 // 以便管理员进行审核操作
 func (m *Images) GetOne(instance interface{}) error {
